27505d88943f543d48463fcf378284e559d41c3a
Ren4t <ren4tab@yandex.ru>
2021-07-01 15:15:11 +0300

    homework3

---
+++ src/main/java/ru/geekbrains/summer/market/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.summer.market;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.geekbrains.summer.market")
+public class AppConfig {
+}
