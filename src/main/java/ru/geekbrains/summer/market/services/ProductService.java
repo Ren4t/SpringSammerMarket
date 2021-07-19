5e87810c1f2c20713d8fb60ea4d07cb60729e330
ren4t <ren4tab@gmail.com>
2021-07-19 15:08:39 +0300

    homework8

--- src/main/java/ru/geekbrains/summer/market/services/ProductService.java
+++ src/main/java/ru/geekbrains/summer/market/services/ProductService.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.summer.market.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.summer.market.model.Product;
 import ru.geekbrains.summer.market.repositories.ProductRepository;
@@ -23,5 +25,9 @@ public class ProductService {
 
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    public Page<Product> findPage(int pageIndex, int pageSize){
+        return productRepository.findAll(PageRequest.of(pageIndex, pageSize));
     }
 }
