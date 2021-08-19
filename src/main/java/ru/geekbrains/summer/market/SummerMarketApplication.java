eb5f1896d630f149249475a90d9dff411060ad85
Ren4t <ren4tab@yandex.ru>
2021-08-19 16:18:19 +0300

    Homework13_14

--- src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
+++ src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SummerMarketApplication {
-	// Домашнее задание:
-	// ----
-	// | 1. Добавить на фронтенд пункт Итого с price корзины
-	// | 2. Доработать кнопки удалить товар из корзины и сделать кнопку "очистить корзину"
-	// | 3. Добавить возможность в корзине менять количество товаров в позиции с помощью кнопок +/-
-	// ----
-	// 4. * Попробуйте сохранить заказ в БД
+	// Домашнее задание:13-14
+	// черновик
 
 	public static void main(String[] args) {
 		SpringApplication.run(SummerMarketApplication.class, args);
